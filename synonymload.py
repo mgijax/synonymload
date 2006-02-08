1599dbcb8969c320331ed0ed334f8ea4c6b5d674
Lori Corbani <Lori.Corbani@jax.org>
2006-02-08 13:28:45 +0000

    converted to MGI_Synonym

--- synonymload.py
+++ synonymload.py
@@ -4,47 +4,50 @@
 #
 # Purpose:
 #
-#	To load new marker synonym records into DB structures:
-#
-#	MRK_Other
+#	To load new records into MGI Synonym
+#		. MGI_Synonym
 #
 # Assumes:
 #
-#	That no one else is adding synonym records to the database.
+#	That no one else is adding Nomen records to the database.
 #
 # Side Effects:
 #
 #	None
 #
-# Input(s):
+# Input:
 #
 #	A tab-delimited file in the format:
-#		field 1: MGI Acc ID of Symbol
-#		field 2: Synonym|J:#####
-#		field 3: Synonym|J:#####
-#		etc.
+#		field 1: Object Accession ID
+#		field 2: Synonym
+#		field 3: Synonym Type (from MGI_Synonym_Type)
+#		field 4: Reference (J:)
+#		field 5: Submitted By
 #
 # Parameters:
 #	-S = database server
 #	-D = database
 #	-U = user
 #	-P = password file
-#	-M = mode (load, preview)
-#	-I = input file of synonym data
+#	-M = mode (load, preview, reload)
+#	-O = object type of Accession ID (_MGIType_key)
+#	-I = input file
 #
 #	processing modes:
-#
-#		load - create the Synonyms
+#		load - load the data
 #
 #		preview - perform all record verifications but do not load the data or
 #		          make any changes to the database.  used for testing or to preview
 #			  the load.
 #
+#		reload - delete existing synonyms for specified object type/reference
+#	                 (assumes that that same reference is used for each synonym)
+#
 # Output:
 #
 #       1 BCP file:
 #
-#       MRK_Other.bcp               Synonym records
+#       MGI_Synonym.bcp
 #
 #	Diagnostics file of all input parameters and SQL commands
 #	Error file
@@ -55,20 +58,30 @@
 #		if mode = load:  process records
 #		if mode = preview:  set "DEBUG" to True
 #
+#	2. Load Synonym Types into dictionary for quicker lookup.
+#
 #	For each line in the input file:
 #
-#	1.  Verify the Marker Acc ID is valid.  Duplicates are reported as errors.
+#	1.  Verify the Accession ID is valid.
 #	    If the verification fails, report the error and skip the record.
 #
-#	2.  Verify the J: is valid.
-#	    If the verification fails, report the error and stop.
+#	2.  Verify the Synonym Type is valid.
+#	    If the verification fails, report the error and skip the record.
 #
-#	3.  Create MRK_Other record for the Marker.
+#	3.  Verify the J: is valid.
+#	    If the verification fails, report the error and skip the record.
+#	    If the verification succeeeds, store the Jnum/Key pair in a dictionary
+#	    for future reference.
+#
+#	4.  Verify the Submitted By is provided (i.e. is not null).
+#	    If the verification fails, report the error and skip the record.
+#
+#	5.  Create the Synonym record.
 #
 # History:
 #
-# lec	03/03/2004
-#	- created TR 5613
+# 02/08/2006	lec
+#	- converted from MRK_Other synonym load; primarily for JRS cutover
 #
 '''
 
@@ -78,29 +91,34 @@ import string
 import getopt
 import db
 import mgi_utils
+import accessionlib
 import loadlib
 
 #globals
 
 DEBUG = 0		# set DEBUG to false unless preview mode is selected
+bcpon = 1		# can the bcp files be bcp-ed into the database?  default is yes.
 
 inputFile = ''		# file descriptor
+outputFile = ''		# file descriptor
 diagFile = ''		# file descriptor
 errorFile = ''		# file descriptor
-
 synFile = ''		# file descriptor
 
 diagFileName = ''	# file name
 errorFileName = ''	# file name
 passwordFileName = ''	# file name
-
 synFileName = ''	# file name
 
 mode = ''		# processing mode
+mgiTypeKey = 0		# ACC_MGIType._MGIType_key
+synKey = 0		# MGI_Synonym._Synonym_key
+userKey = 0
 
-bcpdelim = "|"
+synTypeDict = {}	# dictionary of synonym types for quick lookup
+referenceDict = {}	# dictionary of references for quick lookup
 
-loaddate = loadlib.loaddate	# current date
+loaddate = loadlib.loaddate
 
 def showUsage():
 	'''
@@ -138,6 +156,7 @@ def exit(status, message = None):
  
 	try:
 		inputFile.close()
+		outputFile.close()
 		diagFile.write('\n\nEnd Date/Time: %s\n' % (mgi_utils.date()))
 		errorFile.write('\n\nEnd Date/Time: %s\n' % (mgi_utils.date()))
 		diagFile.close()
@@ -162,12 +181,11 @@ def init():
 	#
 	'''
  
-	global inputFile, diagFile, errorFile, errorFileName, diagFileName, passwordFileName
-	global synFile, synFileName
-	global mode
+	global inputFile, outputFile, diagFile, errorFile, errorFileName, diagFileName, passwordFileName
+	global synFileName, synFile, mode, synKey, mgiTypeKey
  
 	try:
-		optlist, args = getopt.getopt(sys.argv[1:], 'S:D:U:P:M:I:')
+		optlist, args = getopt.getopt(sys.argv[1:], 'S:D:U:P:M:O:I:')
 	except:
 		showUsage()
  
@@ -180,8 +198,6 @@ def init():
 	database = ''
 	user = ''
 	password = ''
-	inputFileName = ''
-	jnum = ''
  
 	for opt in optlist:
                 if opt[0] == '-S':
@@ -194,6 +210,8 @@ def init():
 			passwordFileName = opt[1]
                 elif opt[0] == '-M':
                         mode = opt[1]
+                elif opt[0] == '-O':
+                        mgiType = opt[1]
                 elif opt[0] == '-I':
                         inputFileName = opt[1]
                 else:
@@ -206,6 +224,7 @@ def init():
 	   user == '' or \
 	   password == '' or \
 	   mode == '' or \
+	   mgiType == '' or \
 	   inputFileName == '':
 		showUsage()
 
@@ -215,14 +234,20 @@ def init():
  
 	fdate = mgi_utils.date('%m%d%Y')	# current date
 	head, tail = os.path.split(inputFileName) 
+        outputFileName = inputFileName + '.out'
 	diagFileName = tail + '.' + fdate + '.diagnostics'
 	errorFileName = tail + '.' + fdate + '.error'
-	synFileName = tail + '.' + fdate + '.MRK_Other.bcp'
+	synFileName = tail + '.MGI_Synonym.bcp'
 
 	try:
 		inputFile = open(inputFileName, 'r')
 	except:
 		exit(1, 'Could not open file %s\n' % inputFileName)
+		
+	try:
+		outputFile = open(outputFileName, 'w')
+	except:
+		exit(1, 'Could not open file %s\n' % outputFileName)
 		
 	try:
 		diagFile = open(diagFileName, 'w')
@@ -249,9 +274,12 @@ def init():
 	diagFile.write('Server: %s\n' % (server))
 	diagFile.write('Database: %s\n' % (database))
 	diagFile.write('User: %s\n' % (user))
+	diagFile.write('Object Type: %s\n' % (mgiType))
 	diagFile.write('Input File: %s\n' % (inputFileName))
 
 	errorFile.write('Start Date/Time: %s\n\n' % (mgi_utils.date()))
+
+	mgiTypeKey = accessionlib.get_MGIType_key(mgiType)
 
 def verifyMode():
 	'''
@@ -268,12 +296,78 @@ def verifyMode():
 	#
 	'''
 
-	global DEBUG
+	global DEBUG, bcpon
 
 	if mode == 'preview':
 		DEBUG = 1
+		bcpon = 0
 	elif mode != 'load':
 		exit(1, 'Invalid Processing Mode:  %s\n' % (mode))
+
+def verifySynonymType(synType, lineNum):
+	'''
+	# requires:
+	#	synType - the Synonym Type
+	#	lineNum - the line number of the record from the input file
+	#
+	# effects:
+	#	verifies that:
+	#		the Synonym Type exists 
+	#	writes to the error file if the Synonym Type is invalid
+	#
+	# returns:
+	#	0 if the Synonym Type is invalid
+	#	Synonym Type Key if the Synonym Type is valid
+	#
+	'''
+
+	synTypeKey = 0
+
+	if synTypeDict.has_key(synType):
+		synTypeKey = synTypeDict[synType]
+	else:
+		errorFile.write('Invalid Synonym Type (%d) %s\n' % (lineNum, synType))
+		synTypeKey = 0
+
+	return(synTypeKey)
+
+def setPrimaryKeys():
+	'''
+	# requires:
+	#
+	# effects:
+	#	Sets the global primary keys values needed for the load
+	#
+	# returns:
+	#	nothing
+	#
+	'''
+
+	global synKey
+
+        results = db.sql('select maxKey = max(_Synonym_key) + 1 from MGI_Synonym', 'auto')
+        if results[0]['maxKey'] is None:
+                synKey = 1000
+        else:
+                synKey = results[0]['maxKey']
+
+def loadDictionaries():
+	'''
+	# requires:
+	#
+	# effects:
+	#	loads global dictionaries: statusDict, logicalDBDict
+	#	for quicker lookup
+	#
+	# returns:
+	#	nothing
+	'''
+
+	global synTypeDict
+
+	results = db.sql('select _SynonymType_key, synonymType from MGI_SynonymType where _Object_key = %s' % (mgiTypeKey), 'auto')
+	for r in results:
+		synTypeDict[r['synonymType']] = r['_SynonymType_key']
 
 def processFile():
 	'''
@@ -288,13 +382,9 @@ def processFile():
 	#
 	'''
 
+	global synKey
+
 	lineNum = 0
-
-	# Get next available primary key
-
-	results = db.sql('select maxKey = max(_Other_key) + 1 from MRK_Other', 'auto')
-	otherKey = results[0]['maxKey']
-
 	# For each line in the input file
 
 	for line in inputFile.readlines():
@@ -305,55 +395,39 @@ def processFile():
 		# Split the line into tokens
 		tokens = string.split(line[:-1], '\t')
 
-		markerID = tokens[0]
-		markerKey = loadlib.verifyMarker(markerID, lineNum, errorFile)
+		try:
+			accID = tokens[0]
+			synonym = tokens[1]
+			synType = tokens[2]
+			jnum = tokens[3]
+			user = tokens[4]
+		except:
+			exit(1, 'Invalid Line (%d): %s\n' % (lineNum, line))
 
-		# for each synonym/reference pair...
+		objectKey = accessionlib.get_Object_key(accID, _MGIType_key = mgiTypeKey)
+		synTypeKey = verifySynonymType(synType, lineNum, errorFile)
+		referenceKey = loadlib.verifyReference(jnum, lineNum, errorFile)
+		userKey = loadlib.verifyUser(user, lineNum, errorFile)
 
-		for t in tokens[1:]:
+		if objectKey == 0 or \
+			synTypeKey == 0 or \
+			referenceKey == 0 or \
+			userKey == 0:
 
-			[synonym, refID] = string.split(t, '|')
+			# set error flag to true
+			error = 1
 
-			# get the reference key
-			refKey = loadlib.verifyReference(refID, lineNum, errorFile)
+		# if errors, continue to next record
+		if error:
+			continue
 
-			if markerKey == 0 or refKey == 0:
-                	# set error flag to true
-                		error = 1
+		# if no errors, process
 
-			# if errors, continue to next record
-			if error:
-				continue
-
-			# if no errors, process
-
-			bcpWrite(synFile, [otherKey, markerKey, refKey, synonym, loaddate, loaddate])
-			otherKey = otherKey + 1
+		synFile.write('%d|%d|%d|%d|%s|%s|%s|%s|%s|%s\n' \
+			% (synKey, objectKey, mgiTypeKey, synTypeKey, referenceKey, synonym, userKey, userKey, loaddate, loaddate))
+		synKey = synKey + 1
 
 #	end of "for line in inputFile.readlines():"
-
-def bcpWrite(fp, values):
-	'''
-	#
-	# requires:
-	#	fp; file pointer of bcp file
-	#	values; list of values
-	#
-	# effects:
-	#	converts each value item to a string and writes out the values
-	#	to the bcpFile using the appropriate delimiter
-	#
-	# returns:
-	#	nothing
-	#
-	'''
-
-	# convert all members of values to strings
-	strvalues = []
-	for v in values:
-		strvalues.append(str(v))
-
-	fp.write('%s\n' % (string.join(strvalues, bcpdelim)))
 
 def bcpFiles():
 	'''
@@ -367,18 +441,20 @@ def bcpFiles():
 	#
 	'''
 
-	synFile.close()
+	bcpdelim = "|"
 
-	cmd1 = 'cat %s | bcp %s..%s in %s -c -t\"%s" -S%s -U%s' \
-		% (passwordFileName, db.get_sqlDatabase(), \
-	   	'MRK_Other', synFileName, bcpdelim, db.get_sqlServer(), db.get_sqlUser())
-
-	diagFile.write('%s\n' % cmd1)
-
-	if DEBUG:
+	if DEBUG or not bcpon:
 		return
 
-	os.system(cmd1)
+	synFile.close()
+
+	bcp1 = 'cat %s | bcp %s..%s in %s -c -t\"%s" -S%s -U%s' \
+		% (passwordFileName, db.get_sqlDatabase(), \
+	   	'MGI_Synonym', synFileName, bcpdelim, db.get_sqlServer(), db.get_sqlUser())
+
+	diagFile.write('%s\n' % bcp1)
+
+	os.system(bcp1)
 
 #
 # Main
@@ -386,6 +462,8 @@ def bcpFiles():
 
 init()
 verifyMode()
+setPrimaryKeys()
+loadDictionaries()
 processFile()
 bcpFiles()
 exit(0)
