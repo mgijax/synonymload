1d7496759af51324c782e06e6e4ddce56c2f013b
Lori Corbani <lori.corbani@jax.org>
2022-01-11 13:43:44 -0500

    autosequence changes

--- synonymload.py
+++ synonymload.py
@@ -283,11 +283,8 @@ def setPrimaryKeys():
 
 	global synKey
 
-        results = db.sql('select max(_Synonym_key) + 1 from MGI_Synonym as maxKey', 'auto')
-        if results[0]['maxKey'] is None:
-                synKey = 1000
-        else:
-                synKey = results[0]['maxKey']
+        results = db.sql(' select nextval(mgi_synonym_seq') as maxKey ', 'auto')
+        synKey = results[0]['maxKey']
 
 def loadDictionaries():
 	# requires:
@@ -412,6 +409,9 @@ def bcpFiles():
 	diagFile.write('%s\n' % bcp1)
 	os.system(bcp1)
 	db.commit()
+
+        db.sql(''' select setval('mgi_synonym_seq', (select max(_Synonym_key) from MGI_Synonym)) ''', None)
+        db.commit()
 
 #
 # Main
